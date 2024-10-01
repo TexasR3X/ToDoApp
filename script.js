01771e822667d10087aab2a4524b7e83ef8d3a5b
Texas R3X <texasr3xphone@gmail.com>
2024-10-01 10:26:50 -0600

    You can now add list items to a list in the HTML.

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ class List {
             this.list.push(item);
         });
     }
+
+    index(index) {
+        return this.list[index]
+    }
 }
 
 function appendElm(parent, child) {
@@ -20,23 +24,17 @@ function appendElm(parent, child) {
     document.querySelector(parent).appendChild(newElm);
 }
 
-// function addListItem() {
-//     const newElm = document.createElement("li");
-//     body.appendChild(newElm);
-// }
-
-function addListItemTo(listSelector) {
-    const newElm = document.createElement("li");
-    document.querySelector(listSelector).appendChild(newElm);
-}
-
-
-// document.appendElm = function () {
-//     console.log("2 document:", document);
-
-// }
-
-
-addItemElms[0].addEventListener("click", function () {
-    
+const buttons = document.querySelectorAll(".addItem");
+buttons.forEach((button, i) => {
+    console.log("button:", button);
+    button.addEventListener("click", () => addListItem(i + 1));
 });
+
+function addListItem(listId) {
+    const newElm = document.createElement("li");
+    newElm.innerHTML = `<input type="text">`;
+
+    document.querySelector(`#list-${listId} ul`).appendChild(newElm);
+    // lists[0].index(0);
+    document.querySelector(`#list-${listId} ul li:last-child input`).focus();
+}
