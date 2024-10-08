d86d9b216ebd802dea3f37943a1969f31939047b
Texas R3X <texasr3xphone@gmail.com>
2024-10-08 11:18:22 -0600

    I restructured how the data is stored in the ListOfLists class, and I slightly altered the structure of the data in local storage.

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,25 +1,31 @@
 // ============================== classes.js is stores all of the classes in main.js ============================== //
 
 // The ListOfLists class will be used for the listOfLists variable in main.js
-// A ListOfLists object will mimic the following format: [{}, {}, {}, ...]
-// It will technically be in the following format: ListOfLists {0: List {}, 1: List {}, 2: List {}, ...}
-    // On the outside, the ListOfLists class will mimic an array.
-    // For the most part, each element of the fake array will be a List object.
+// A ListOfLists object will have the following structure:
+    // this.lists
+        // This stores the do-to lists.
+        // Children: List class instances
+    // this.length
+        // This will equal the number of lists in this.lists
 export class ListOfLists {
-    constructor(listOfLists) {
-        if (typeof listOfLists) { listOfLists = JSON.parse(listOfLists); }
+    constructor(lists) {
+        // Properties:
+            // this.lists
+                // Children: List class
+            // this.length
 
-        let count = 0;
-        for (let i = 0; i < listOfLists.length; i++) {
-            this[i] = new List(listOfLists[i].name, listOfLists[i].id, listOfLists[i].items);
-            count++;
-        }
-        this.length = count;
+        // Code to build the properties:
+        if (typeof lists) { lists = JSON.parse(lists); }
+
+        this.lists = {};
+        for (let list of lists) { this.lists[list.name] = new List(list.name, list.items); }
+
+        this.length = lists.length;
     }
 
     toHTML() {
         let output = "";
-        for (let i = 0; i < this.length; i++) { output += ` ${this[i].toHTML()} `; }
+        for (let list of Object.values(this.lists)) { output += ` ${list.toHTML()} `; }
         return output += `
             <div class="add-new-list">
                 <h2>Create New List</h2>
@@ -27,23 +33,19 @@ export class ListOfLists {
             </div>
         `;
     }
-    getList(id) {
-        for (let list in this) {
-            if (list.id === id) { return list; } }
-    }
+    getListByName(name) { for (let listName in this.lists) { if (listName === name) { return this.lists[listName]; } } }
+    getListByHTMLId(id) { return this.getListByName(id.slice(10)); }
 }
 
 export class List {
-    constructor(name, listId, items) {
+    constructor(name, items) {
         this.name = name;
-        this.id = listId;
         this.items = items;
     }
-
+    // This method is never used and maybe should be deleted.
     toObjectLiteral() {
         return {
             name: this.name,
-            id: this.id,
             items: this.items
         };
     }
@@ -52,13 +54,14 @@ export class List {
         this.items.forEach((item) => { ulElm += ` <li>${item}</li> `; });
 
         return `
-            <div id="list-${this.id}" class="list-container">
-                <h2>${this.name}</h2>
+            <div id="list-name-${this.name}" class="list-container">
+                <h2>${this.name.replaceAll("_", " ")}</h2>
                 <ul>${ulElm}</ul>
                 <button class="add-item">Add Item</button>
             </div>
         `;
     }
+    push(item) { this.items.push(item); }
 }
 
 // Test code to add data to local storage.
@@ -67,17 +70,17 @@ export class List {
 //     `[
 //         {
 //             "name": "Homework",
-//             "id": 100,
-//             "list": ["a", "b", "c"]
+//             "order": null,
+//             "items": ["a", "b", "c"]
 //         },
 //         {
-//             "name": "Another name that will not be seen",
-//             "id": 2,
-//             "list": [1, 2.817, 3.14, 5]
+//             "name": "Another_List",
+//             "order": null,
+//             "items": ["1", "2.817", "3.14, 5"]
 //         },
 //         {
-//             "name": "Web Dev",
-//             "id": 21,
-//             "list": ["All of the work!"]
+//             "name": "Web_Dev",
+//             "order": null,
+//             "items": ["All of the work!"]
 //         }
-//     ]`);
+//      ]`);
